fc090af3fded9eec7be7db747e1185dd3015c913
colocky <callmeadam.ar@gmail.com>
2026-05-22 11:22:49 -0500

    Create PizzaSize

---
+++ src/main/java/com/pizzaworld/models/PizzaSize.java
@@ -0,0 +1,30 @@
+package com.pizzaworld.models;
+
+public class PizzaSize {
+    private String name;
+    private int inches;
+    private double basePrice;
+
+    public PizzaSize(String name, int inches, double basePrice) {
+        this.name = name;
+        this.inches = inches;
+        this.basePrice = basePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getInches() {
+        return inches;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + inches + "\" - $" + String.format("%.2f", basePrice);
+    }
+}
