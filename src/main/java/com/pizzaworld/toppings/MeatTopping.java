a8ba941402c6eaf19aaa48e2a6ae8732d1a68f88
colocky <callmeadam.ar@gmail.com>
2026-05-26 09:43:40 -0500

    Add MeatTopping.java

---
+++ src/main/java/com/pizzaworld/toppings/MeatTopping.java
@@ -0,0 +1,35 @@
+package com.pizzaworld.toppings;
+
+import com.pizzaworld.models.PizzaSize;
+
+public class MeatTopping extends Topping {
+    public MeatTopping(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getPrice(PizzaSize size, Boolean extra) {
+        double price = 0;
+
+        if (size.getInches() == 8) {
+            price = 1.00;
+        } else if (size.getInches() == 12) {
+            price = 2.00;
+        } else if (size.getInches() == 16) {
+            price = 3.00;
+        }
+
+        if (extra) {
+            if (size.getInches() == 8) {
+                price += 0.50;
+            } else if (size.getInches() == 12) {
+                price += 1.00;
+            } else if (size.getInches() == 16) {
+                price += 1.50;
+            }
+        }
+
+        return price;
+    }
+
+}
