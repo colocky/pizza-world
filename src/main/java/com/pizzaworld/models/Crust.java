fd294c1f5b965c7fca2225732c74537096cd4726
colocky <callmeadam.ar@gmail.com>
2026-05-26 09:31:19 -0500

    Add Crust.java

---
+++ src/main/java/com/pizzaworld/models/Crust.java
@@ -0,0 +1,17 @@
+package com.pizzaworld.models;
+
+public class Crust {
+    private String name;
+
+    public Crust(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+            return name;
+    }
+}
