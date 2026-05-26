3fba00bb8d1dc71d12696c994a64e0dc7b5044c4
colocky <callmeadam.ar@gmail.com>
2026-05-26 09:33:22 -0500

    Add Sauce.java

---
+++ src/main/java/com/pizzaworld/models/Sauce.java
@@ -0,0 +1,17 @@
+package com.pizzaworld.models;
+
+public class Sauce {
+    private String name;
+
+    public Sauce(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
