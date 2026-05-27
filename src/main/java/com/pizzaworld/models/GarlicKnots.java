069d1f12281d2de82207e8468efdf3423936da6a
colocky <callmeadam.ar@gmail.com>
2026-05-27 10:51:35 -0500

    Add GarlicKnots.java

---
+++ src/main/java/com/pizzaworld/models/GarlicKnots.java
@@ -0,0 +1,27 @@
+package com.pizzaworld.models;
+
+public class GarlicKnots implements OrderItem {
+
+    private static final double price = 1.50;
+    private int quantity;
+    public GarlicKnots(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    @Override
+    public String getName() {
+        return "Garlic Knots";
+    }
+    @Override
+    public double getPrice() {
+        return price * quantity;
+    }
+    @Override
+    public String getDetails() {
+        return getName() + "\n" +
+                " Quantity: " + quantity + "\n" +
+                " Price: $" + String.format("%.2f", getPrice());
+    }
+}
