7ac0f833cc178a9ee01354997addb0fc00a1ce6f
colocky <callmeadam.ar@gmail.com>
2026-05-27 14:35:54 -0500

    Add ReceiptWriter.java

---
+++ src/main/java/com/pizzaworld/services/ReceiptWriter.java
@@ -0,0 +1,35 @@
+package com.pizzaworld.services;
+
+import com.pizzaworld.models.Order;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.io.*;
+
+
+public class ReceiptWriter {
+    private String receiptDirectory;
+
+    public ReceiptWriter(String receiptDirectory) {
+        this.receiptDirectory = receiptDirectory;
+    }
+
+    public void saveReceipt(Order order) {
+        File folder = new File(receiptDirectory);
+
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String fileName = LocalDateTime.now().format(formatter) + ".txt";
+        File receiptFile = new File(folder, fileName);
+        try (FileWriter writer = new FileWriter(receiptFile)) {
+            writer.write(order.getOrderDetails());
+            System.out.println("Receipt saved: " + receiptFile.getPath());
+        } catch (IOException e) {
+            System.out.println("Could not save receipt.");
+            System.out.println(e.getMessage());
+        }
+    }
+}
